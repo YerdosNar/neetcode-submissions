9838537e070c4b45d6b28328658b89fe2bb84497
Narzhigitov Yerdos <128128393+YerdosNar@users.noreply.github.com>
2026-08-02 06:21:58 +0900

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length()-1;
+        while (l < r) {
+            char lc = s.charAt(l);
+            char rc = s.charAt(r);
+            if (lc >= 'A' && lc <= 'Z') lc += 32;
+            if ((lc < 'a' || lc > 'z') && (lc < '0' || lc > '9')) {l++;continue;}
+            if (rc >= 'A' && rc <= 'Z') rc += 32;
+            if ((rc < 'a' || rc > 'z') && (rc < '0' || rc > '9')) {r--;continue;}
+
+            System.out.println("L: "+lc+", R: "+rc);
+            if (lc != rc) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
