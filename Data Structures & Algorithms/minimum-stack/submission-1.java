44e3b98a29586b976a934421c985592032162735
Narzhigitov Yerdos <128128393+YerdosNar@users.noreply.github.com>
2026-08-04 17:42:29 +0900

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,31 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        int min = val;
+        if (!stack.isEmpty()) {
+            min = Math.min(val, minStack.peek());
+        }
+        minStack.push(min);
+        stack.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
