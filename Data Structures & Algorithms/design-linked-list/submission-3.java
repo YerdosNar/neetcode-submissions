8723a204cb3574e26c79d5f4e40321a1392b4b39
Narzhigitov Yerdos <128128393+YerdosNar@users.noreply.github.com>
2026-08-08 23:50:10 +0900

    Add: design-linked-list - submission-3

---
+++ Data Structures & Algorithms/design-linked-list/submission-3.java
@@ -0,0 +1,84 @@
+class Node {
+    int val;
+    Node next;
+    Node (int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+    Node (int val) {
+        this(val, null);
+    }
+}
+
+class MyLinkedList {
+    private Node head;
+    private int size;
+
+    public MyLinkedList() {
+        head = new Node(0, null);
+        size = 0;
+    }
+
+    private Node getNode(int index) {
+        Node cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+    
+    public int get(int index) {
+        if (index >= size || index < 0) return -1;
+        return getNode(index).next.val;
+    }
+    
+    private void printAll(String msg) {
+        System.out.println("START "+msg);
+
+        Node cur = head;
+        while (cur != null) {
+            System.out.println(+cur.val);
+            cur = cur.next;
+        }
+        System.out.println("END "+msg);
+        System.out.println();
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+        printAll("ATHEAD: ");
+    }
+    
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+        printAll("ATTAIL: ");
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if (index > size) return;
+
+        Node prev = getNode(index);
+        Node newN = new Node(val, prev.next);
+        prev.next = newN;
+        size++;
+        printAll("ATIDX: ");
+    }
+    
+    public void deleteAtIndex(int index) {
+        if (index >= size) return;
+
+        Node node = getNode(index);
+        node.next = node.next.next;
+        size--;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
